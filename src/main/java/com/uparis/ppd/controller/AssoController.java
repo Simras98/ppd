8d73e017fd77790239574914a66a4b9b9155e1c2
Simon WEBER <simon_weber@carrefour.com>
2021-02-13 19:37:08 +0100

    Rename controller file

--- src/main/java/com/uparis/ppd/controller/AssoController.java
+++ src/main/java/com/uparis/ppd/controller/AssoController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import javax.sql.DataSource;
 
 @Controller
-public class PpdController {
+public class AssoController {
 
   @Autowired private DataSource dataSource;
 
